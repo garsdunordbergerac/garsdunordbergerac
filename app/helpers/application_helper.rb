b9c1f5749c5cbde4a9451b83966b013f7a9b551b
Kevin <kevin@koboyz.org>
2013-05-09 12:40:26 +0200

    Select the best event year based on the current year

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,35 +1,6 @@
 module ApplicationHelper
-  def contact_map
-    gmaps(contact_map_options.merge(contact_markers))
-  end
-
-private
-
-  def contact_map_options
-    {
-      map_options: {
-        type:   'ROADMAP',
-        styles: [
-          { stylers: [{ saturation: -100 }]}
-        ],
-        mapTypeControl: false,
-        navigationControl: false,
-        streetViewControl: false,
-        scrollwheel: false,
-        scaleControl: false,
-        zoom: 1,
-        auto_adjust: true
-      }.to_json
-    }
-  end
-
-  def contact_markers
-    {
-      markers: {
-        data: [
-          { description: '', title: '', lng: '0.4833920', lat: '44.8538060', picture: '', width: '', height: '' }
-        ].to_json
-      }
-    }
+  def current_event_year(years)
+    current_year = Date.today.year
+    years.include?(current_year.to_s) ? current_year : years.first
   end
 end
