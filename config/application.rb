3030336d8b35ce55d96162fdc091e4d6bf757364
Kevin <kevin@koboyz.org>
2013-05-13 12:59:15 +0200

    Add logs for unicorn

--- config/application.rb
+++ config/application.rb
@@ -68,5 +68,6 @@ module GarsDuNordBergerac
       enable_starttls_auto: ENV['EMAIL_TLS']
     }
     config.assets.initialize_on_precompile = false
+    config.logger = Logger.new(STDOUT)
   end
 end
