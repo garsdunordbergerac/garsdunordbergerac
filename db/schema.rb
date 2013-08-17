84453bdbbbaaa8c8fb3825f3768093f37a7d132a
Kevin <kevin@koboyz.org>
2013-08-17 15:05:07 +0200

    [migrations] Refactor contact displaying

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130713180700) do
+ActiveRecord::Schema.define(:version => 20130817104507) do
 
   create_table "contacts", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130713180700) do
     t.string   "position"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "phone"
   end
 
   create_table "contents", :force => true do |t|
